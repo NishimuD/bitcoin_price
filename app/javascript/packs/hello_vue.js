81e1e05925ad59e82d591d9dd8bd0ed342c6c36d
nishimura-d <gjmt45@icloud.com>
2019-04-11 01:57:12 +0900

    show bitcoin_price

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -8,66 +8,31 @@
 import Vue from 'vue'
 import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'))
-  const app = new Vue({
-    el,
-    render: h => h(App)
-  })
-
-  console.log(app)
+var app = new Vue({
+  el: '#app',
+  data: {
+    bpi: null,
+    hasError: false,
+    loading :true
+  },
+  mounted: function(){
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response){
+      // console.log(response.data.bpi)
+      // console.log(response.data.bpi.USD.rate_float)
+      this.bpi = response.data.bpi
+    }.bind(this))
+    .catch(function(error){
+      console.log(error)
+      this.hasError = true
+    }.bind(this))
+    .finally(function(){
+      this.loading = false
+    }.bind(this))
+  },
+  filters: {
+    currencyDecimal(value) {
+      return value.toFixed(2)
+    }
+  }
 })
-
-
-// The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
-
-
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
-//
-//
-//
-// If the project is using turbolinks, install 'vue-turbolinks':
-//
-// yarn add vue-turbolinks
-//
-// Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
